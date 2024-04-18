bcc7d7ec3f429cb87c25b043311ae660d85a350a
kakanvk <caoka5265@gmail.com>
2024-04-18 10:26:43 +0700

    Prime Check

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+public class App {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 17; // Số cần kiểm tra
+        if (isPrime(number)) {
+            System.out.println(number + " is prime.");
+        } else {
+            System.out.println(number + " is not prime.");
+        }
+    }
+}
